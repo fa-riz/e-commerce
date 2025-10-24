ca6bf027582eb22dd840438385e3d138c23daec8
Muhammed Fariz P A <muhammedfarizpa@gmail.com>
2025-10-24 16:10:26 +0530

    generated checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/checkout" element={<div></div>}></Route>
+        <Route path="/checkout" element={<CheckoutPage />}></Route>
       </Routes>
     </>
   );
